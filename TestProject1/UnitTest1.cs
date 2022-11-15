ac0f52814fcde571f74f4ee8f28d37710850ec3c
kev <kevinkien47@gmail.com>
2022-11-15 14:26:15 +0700

    message

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -13,5 +13,29 @@ namespace TestProject1
             Assert.AreEqual('A', student.getLetterScore());
 
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Student student = new Student();
+            student.Score = 7;
+            Assert.AreEqual('B', student.getLetterScore());
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Student student = new Student();
+            student.Score = 5;
+            Assert.AreEqual('C', student.getLetterScore());
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Student student = new Student();
+            student.Score = 3;
+            Assert.AreEqual('E', student.getLetterScore());
+
+        }
     }
 }
